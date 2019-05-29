2fac42060d45d8543a9657de1e405511469b33fb
yunwei <yunwei@ruitone.com.cn>
2019-05-29 19:28:33 +0800

    添加新浪股票api

--- crawl_study/crawl_study/spiders/test.py
+++ crawl_study/crawl_study/spiders/test.py
@@ -1,13 +1,63 @@
 from lxml import etree
 import requests
+from crawl_study.spiders.utils import timeutil
+import os
+import hashlib
+import threading
+import redis
 
 
-test_url = "http://blog.sina.com.cn/twocold"
-resp = requests.get(test_url).content
-print(resp.decode('utf-8'))
-html = etree.HTML(resp)
+yzm_url = "http://wapzt.189.cn/wap/getPicRandomCode.do"
+test_url = "http://wapzt.189.cn/wap/getPicRandomCode.do?param=1559117050420&sessionid=4765139bfbba460b9caf382921378663&type=2"
+redis_client = redis.Redis(host = "192.168.88.200", port = 6379, password = "redis")
+redis_client = redis.StrictRedis()
+redis_pool = redis.ConnectionPool(host = '', port = 6379, password = "", decode_responses = True)
+redis_client = redis.Redis(connection_pool= redis_pool)
 
-#titles = html.xpath('//*[@class="blog_title"]/a/text()')
-titles = html.xpath('//*[@class="tag SG_txtc"]/text()[1]')
-#titles = html.xpath('//*[@id="t_10001_4701280b0102wruo"]/a/text()')
-print(titles)
+def get_md5(file_path):
+    md5 = None
+    if os.path.isfile(file_path):
+        with open(file_path , 'rb') as f:
+            md5_ojb = hashlib.md5()
+            md5_ojb.update(f.read())
+            hash_code = md5_ojb.hexdigest()
+            md5 = str(hash_code).lower()
+    return md5
+
+
+def download_yzm(timestamp):
+    is_repeat = False
+    redis_file_path = None
+    #timestamp = timeutil.utc13()
+    #resp = requests.get(yzm_url + timestamp)
+    resp = requests.get(test_url)
+    yzm_name = 'I:\\yzm_img\\' + timestamp + '.jpg'
+    if resp.status_code == 200:
+        with open(yzm_name,'wb') as f:
+            f.write(resp.content)
+        md5 = get_md5(yzm_name)
+    redis_file_path = redis_client.get(md5)
+    if redis_file_path:
+        is_repeat = True
+        print("当前文件%s与%smd5值相同"%(yzm_name,redis_file_path))
+    else:
+        redis_client.set(md5,yzm_name)
+
+
+def run(thread_id):
+    for i in range(999):
+        timestamp = timeutil.utc13() + str(thread_id) + str(i)
+        download_yzm(timestamp)
+
+
+if __name__ == "__main__":
+    thread_num = 50
+    thread_list = []
+    for i in range(thread_num):
+        t = threading.Thread(target = run(i,))
+        thread_list.append(t)
+    for i in thread_list:
+        t.setDaemon(True)
+        t.start()
+    for i in thread_list:
+        t.join()
